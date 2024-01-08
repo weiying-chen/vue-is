57c0f4dcebeb90a4dd182dbd31eac616bb9a07bb
Wei-ying Chen <weiying.alex.chen@gmail.com>
2024-01-08 22:02:38 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Vue Infinite Scrolling
+
+Search population by city or state.
+
+![](https://github.com/weiying-chen/vue-infinite-scrolling/blob/main/screenshot.png)
+
+**[Live Demo](https://vue-infinite-scrolling.vercel.app/)**
+
+## Installation
+
+```bash
+# Install dependencies
+npm install
+```
+
+## Usage
+
+```bash
+# Run app
+npm run dev
+
+# Build app
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import InfiniteScroll from './components/InfiniteScroll.vue';
+</script>
+
+<template>
+  <div class="app">
+    <InfiniteScroll />
+  </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap');
+@import './styles.css';
+
+.app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 1200px;
+  margin: auto;
+  padding: 20px;
+
+  .error {
+    color: #ff0000;
+  }
+}
+</style>
